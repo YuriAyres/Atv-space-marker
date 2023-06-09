c10f5e70acacf538c9d36f009fc7030f340e13af
YuriAyres <yuriadp7@gmail.com>
2023-06-09 15:03:49 -0300

    Marcação das linhas

--- main.py
+++ main.py
@@ -14,17 +14,30 @@ fundo = pygame.image.load("bg.jpg")
 pygame.mixer.music.load("Space_Machine_Power.mp3")
 pygame.mixer.music.play(-1)
 estrelas = {}
-lista_pos = []
+start_pos = None
 white = (255, 255, 255)
 icone = pygame.image.load("space.png")
 pygame.display.set_icon(icone)
 tela.blit(fundo,(0,0))     
+font2 = pygame.font.Font(None, 25)
+text2 = font2.render("Pressione F10 para Salvar os Pontos", True, white)
+text3 = font2.render("Pressione F11 para Carregar os Pontos", True, white)
+text4 = font2.render("Pressione F12 para Deletar os Pontos", True, white)
+tela.blit(text2,(10,10))
+tela.blit(text3,(10,30))
+tela.blit(text4,(10,50))
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
+            if start_pos is None:
+                        start_pos = pos
+            else:
+                end_pos = pos
+                pygame.draw.line(tela, white, start_pos, end_pos , 2)
+                start_pos = pos                      
             item = simpledialog.askstring("Space","Nome da Estrela:")
             if item == "":
                 item = "desconhecido"+str(pos)
@@ -39,6 +52,7 @@ while running:
                 else:
                     pos2 = sutrair_tupla(pos)
                     tela.blit(text, (pos2))
+            
                     
 
 
