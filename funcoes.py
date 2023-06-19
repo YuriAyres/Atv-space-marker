f705eeb14b9f483bced0a1c63e0e68448ebd3b69
YuriAyres <yuriadp7@gmail.com>
2023-06-19 14:47:39 -0300

    Código quase pronto

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,4 @@
+import pygame
 def analisar_tupla(tupla,valorx,valory):
     primeiro_indice = tupla[0]
     segundo_indice = tupla[1]
@@ -13,10 +14,10 @@ def analisar_tupla(tupla,valorx,valory):
 def sutrair_tupla(tupla):
     primeiro_indice = tupla[0]
     segundo_indice = tupla[1]
-    if primeiro_indice > 950:
+    if primeiro_indice >= 950:
         a = primeiro_indice - 150
     elif primeiro_indice >852:
-        a = primeiro_indice - 70
+        a = primeiro_indice - 100
     elif primeiro_indice:
         a = primeiro_indice
     if segundo_indice:
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
+import pickle
 from tkinter import simpledialog
+from tkinter import messagebox
 from funcoes import analisar_tupla
 from funcoes import sutrair_tupla
 pygame.init()
@@ -14,11 +16,13 @@ fundo = pygame.image.load("bg.jpg")
 pygame.mixer.music.load("Space_Machine_Power.mp3")
 pygame.mixer.music.play(-1)
 estrelas = {}
+nome_arquivo = "estrelas.pkl"
 start_pos = None
 white = (255, 255, 255)
 icone = pygame.image.load("space.png")
 pygame.display.set_icon(icone)
-tela.blit(fundo,(0,0))     
+tela.blit(fundo,(0,0))
+font = pygame.font.Font(None, 20)     
 font2 = pygame.font.Font(None, 25)
 text2 = font2.render("Pressione F10 para Salvar os Pontos", True, white)
 text3 = font2.render("Pressione F11 para Carregar os Pontos", True, white)
@@ -26,24 +30,25 @@ text4 = font2.render("Pressione F12 para Deletar os Pontos", True, white)
 tela.blit(text2,(10,10))
 tela.blit(text3,(10,30))
 tela.blit(text4,(10,50))
+reiniciar_da_tela = False
+
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
         elif event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
             if start_pos is None:
-                        start_pos = pos
+                start_pos = pos
             else:
                 end_pos = pos
-                pygame.draw.line(tela, white, start_pos, end_pos , 2)
+                pygame.draw.line(tela, white, start_pos, end_pos , 1)
                 start_pos = pos                      
             item = simpledialog.askstring("Space","Nome da Estrela:")
             if item == "":
                 item = "desconhecido"+str(pos)
             estrelas[item]= pos
             for key, value in estrelas.items():
-                font = pygame.font.Font(None, 20)
                 text = font.render(item, True, white)
                 pygame.draw.circle(tela, white, pos, 3.5)
                 limite = analisar_tupla(pos,852,550)
@@ -52,9 +57,38 @@ while running:
                 else:
                     pos2 = sutrair_tupla(pos)
                     tela.blit(text, (pos2))
-            
-                    
-
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_F10:
+             with open(nome_arquivo, 'wb') as arquivo:
+                pickle.dump(estrelas, arquivo)
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
+            try:
+                with open(nome_arquivo, 'rb') as arquivo:
+                    estrelas = pickle.load(arquivo)
+                    for key, value in estrelas.items():
+                        text = font.render(key, True, white)
+                        pygame.draw.circle(tela, white, value, 3.5)
+                        limite = analisar_tupla(value,852,550)
+                        if limite == False:
+                            tela.blit(text, (value))
+                        else:
+                            pos2 = sutrair_tupla(value)
+                            tela.blit(text, (pos2))
+                        if start_pos is None:
+                            start_pos = value
+                        else:
+                            end_pos = value
+                            pygame.draw.line(tela, white, start_pos, end_pos , 1)
+                            start_pos = value
+            except:
+                messagebox.showinfo("Erro", "Não existem marcações salvas!")               
+               
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_F12:
+             with open(nome_arquivo, 'w') as arquivo:
+                pass
+        elif pygame.mouse.get_pos() == (500,300):
+            messagebox.showinfo("Distância entre as marcações", "Soma")    
+                         
+ 
 
        
         
